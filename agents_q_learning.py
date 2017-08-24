fc44102e3c25fa44533aa19bd1e6a354c9f82504
Jeff Nyman <jeffnyman@gmail.com>
2017-08-24 05:50:09 -0500

    Q-learning agents added.

--- agents_q_learning.py
+++ agents_q_learning.py
@@ -1,6 +1,7 @@
 from agents_learning import ReinforcementAgent
 from game.feature_extractors import *
 
+import random
 import utilities
 
 
@@ -8,27 +9,47 @@ class QLearningAgent(ReinforcementAgent):
     def __init__(self, **args):
         ReinforcementAgent.__init__(self, **args)
 
-        "*** YOUR CODE HERE ***"
+        # q-values maps (state, action) to q-value
+        self.qvalues = utilities.Counter()
 
     def get_q_value(self, state, action):
-        utilities.raise_not_defined()
+        return self.qvalues[(state, action)]
 
     def compute_value_from_q_values(self, state):
-        utilities.raise_not_defined()
+        policy = self.get_policy(state)
+        value = self.get_q_value(state, policy)
+        return value
 
     def compute_action_from_q_values(self, state):
-        utilities.raise_not_defined()
+        best_action, best_value = (None, float('-inf'))
+        actions = self.get_legal_actions(state)
+
+        for action in actions:
+            qvalue = self.get_q_value(state, action)
+            if qvalue > best_value:
+                best_action, best_value = action, qvalue
+
+        return best_action
 
     def get_action(self, state):
         legal_actions = self.get_legal_actions(state)
         action = None
 
-        utilities.raise_not_defined()
+        if utilities.flip_coin(self.epsilon):
+            action = random.choice(legal_actions)
+        else:
+            action = self.get_policy(state)
 
         return action
 
     def update(self, state, action, next_state, reward):
-        utilities.raise_not_defined()
+        # q-value is updated via q-learning
+        # new_Q(s,a) = (1 - alpha) * old_Q(s,a) + alpha * (reward + discount * future rewards)
+        # new_Q(s,a) = old_Q(s,a) + alpha (reward + discount * future rewards - old_Q(s,a))
+        q_value = self.get_q_value(state, action)
+        next_value = self.get_value(next_state)
+        q_value += self.alpha * (reward + self.discount * next_value - q_value)
+        self.qvalues[(state, action)] = q_value
 
     def get_policy(self, state):
         return self.compute_action_from_q_values(state)
@@ -62,14 +83,36 @@ class ApproximateQAgent(PacmanQAgent):
         return self.weights
 
     def get_q_value(self, state, action):
-        utilities.raise_not_defined()
+        # if no action, return zero
+        if action is None:
+            return 0.0
+
+        # get feature vector
+        feature_vector = self.feat_extractor.get_features(state, action)
+
+        # q-value = sum (weights * feature values) of all features
+        q_value = 0.0
+
+        for feature in feature_vector:
+            q_value += self.weights[feature] * feature_vector[feature]
+
+        return q_value
 
     def update(self, state, action, next_state, reward):
-        utilities.raise_not_defined()
+        # difference = reward + discount * future rewards - q-value
+        q_value = self.get_q_value(state, action)
+        next_value = self.get_value(next_state)
+        difference = reward + self.discount * next_value - q_value
+
+        # update feature weights
+        feature_vector = self.feat_extractor.get_features(state, action)
+
+        for feature in feature_vector:
+            self.weights[feature] += self.alpha * difference * feature_vector[feature]
 
     def final(self, state):
         PacmanQAgent.final(self, state)
 
         if self.episodes_so_far == self.numTraining:
-            "*** YOUR CODE HERE ***"
+            "*** ANY CODE NEEDED HERE? ***"
             pass
